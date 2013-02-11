765b6394a07e0e93dfa6ad193afc2b6a2b5a154a
estebandinamarca <estebandinamarca@gmail.com>
2013-02-11 12:09:44 -0300

    Cambios en Institucional y agragado nuevo Hannover

---
+++ Hannover/js/videos-cortos/videos-cortos.js
@@ -0,0 +1,154 @@
+// EDIFICIO
+var myVideo=document.getElementById("#edificio");
+function playEdificio() { 
+	if (edificio.paused)
+	edificio.play(); 
+	else 
+	edificio.pause(); 
+}
+function CambiarEstiloEdificio(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-edificio-play") {
+	elemento.className = "play-video-corto img-edificio-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-edificio-play";
+	}
+} 
+// HALL
+var myVideo=document.getElementById("hall"); 
+function playHall() { 
+	if (hall.paused) 
+	hall.play(); 
+	else 
+	hall.pause(); 
+} 
+function CambiarEstiloHall(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-hall-play") {
+	elemento.className = "play-video-corto img-hall-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-hall-play";
+	}
+} 
+// CINE
+var myVideo=document.getElementById("cine"); 
+function playCine() { 
+	if (cine.paused) 
+	cine.play(); 
+	else 
+	cine.pause(); 
+} 
+function CambiarEstiloCine(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-cine-play") {
+	elemento.className = "play-video-corto img-cine-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-cine-play";
+	}
+} 
+// GIMNASIO
+var myVideo=document.getElementById("gimnasio"); 
+function playGimnasio() { 
+	if (gimnasio.paused) 
+	gimnasio.play(); 
+	else 
+	gimnasio.pause(); 
+} 
+function CambiarEstiloGimnasio(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-gimnasio-play") {
+	elemento.className = "play-video-corto img-gimnasio-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-gimnasio-play";
+	}
+} 
+// LOUNGE
+var myVideo=document.getElementById("lounge"); 
+function playLounge() { 
+	if (lounge.paused) 
+	lounge.play(); 
+	else 
+	lounge.pause(); 
+}
+function CambiarEstiloLounge(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-lounge-play") {
+	elemento.className = "play-video-corto img-lounge-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-lounge-play";
+	}
+} 
+// PISCINA
+var myVideo=document.getElementById("piscina"); 
+function playPiscina() { 
+	if (piscina.paused) 
+	piscina.play(); 
+	else 
+	piscina.pause(); 
+} 
+function CambiarEstiloPiscina(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-piscina-play") {
+	elemento.className = "play-video-corto img-piscina-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-piscina-play";
+	}
+}
+// QUINCHO
+var myVideo=document.getElementById("quincho"); 
+function playQuincho() { 
+	if (quincho.paused) 
+	quincho.play(); 
+	else 
+	quincho.pause(); 
+} 
+function CambiarEstiloQuincho(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-quincho-play") {
+	elemento.className = "play-video-corto img-quincho-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-quincho-play";
+	}
+}
+// SALA MULTIPLE
+var myVideo=document.getElementById("sala_multiple"); 
+function playMultiple() { 
+	if (sala_multiple.paused) 
+	sala_multiple.play(); 
+	else 
+	sala_multiple.pause(); 
+} 
+function CambiarEstiloMultiple(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-multiple-play") {
+	elemento.className = "play-video-corto img-multiple-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-multiple-play";
+	}
+}
+// DPTO
+var myVideo=document.getElementById("dpto"); 
+function playDpto() { 
+	if (dpto.paused) 
+	dpto.play(); 
+	else 
+	dpto.pause(); 
+}
+function CambiarEstiloDpto(id) {
+	var elemento = document.getElementById(id);
+	if (elemento.className == "play-video-corto img-dpto-play") {
+	elemento.className = "play-video-corto img-dpto-pause";
+	}
+	else {
+	elemento.className = "play-video-corto img-dpto-play";
+	}
+}
+
